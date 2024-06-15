f5b57cbd184abfdb42a595a536c887e0e521e2dd
Henry Pickersgill <htap2@live.co.uk>
2024-06-15 15:32:20 +0100

    finished script, now takes into account current day and the fact that gov website might not be updated for next week

--- bin_day.py
+++ bin_day.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -10,13 +9,41 @@ from selenium.webdriver.common.keys import Keys
 from datetime import datetime
 import smtplib
 import os
-import itertools
 
-def get_bin_day_info(browser, post_code, address, timeout=5):
+def main():
+    """ get the next bin which is to be left out for collection
+        and the corresponding collection date """
+    
+    # directory of this file
+    global_dir = f"{os.path.dirname(os.path.realpath(__file__))}"
 
-    firefoxOptions = Options()
-    # firefoxOptions.headless = True
-    firefoxOptions.binary_location = read_config("firefox_path")
+    # website with bin day information
+    bin_day_url = read_config("bin_day_url")
+
+    # user's post code and address
+    post_code = read_config("post_code")
+    address = read_config("address")
+
+    # firefox browser object
+    service = Service(f"{global_dir}/drivers/geckodriver.exe")
+    firefox_options = webdriver.FirefoxOptions()
+    firefox_options.add_argument("--headless=new")
+    firefox_options.binary_location = read_config("firefox_path")
+    browser = webdriver.Firefox(service=service, options=firefox_options)
+
+    # get bin collection dates
+    bin_dict = get_bin_day_info(browser, bin_day_url, post_code, address)
+
+    # email result to given email address
+    user_gmail = read_config("user_gmail")
+    user_receive = read_config("user_receive")
+    gmail_app_passwd = read_config("gmail_app_passwd")
+    send_bin_dates_to_email(user_gmail, user_receive, gmail_app_passwd, bin_dict)
+
+    # exit firefox instance
+    browser.quit()
+
+def get_bin_day_info(browser, bin_day_url, post_code, address, timeout=5):
     browser.get(bin_day_url)
 
     # wait for cookies pop-up to appear and record the ID of the Deny button. Then click button
@@ -66,57 +93,39 @@ def send_bin_dates_to_email(user_gmail, user_main, gmail_app_passwd, bin_dict):
     server.starttls()
     server.login(user_gmail, gmail_app_passwd)
 
-    # get correct bin from dict
-
-    # format message
+    # get next bin from dict and current date
     message = get_next_bin(bin_dict)
 
     server.sendmail(user_gmail, user_main, message)
 
-def get_next_bin(bin_dict):
-    next_bin = bin_dict['bin_0'][0]
-    next_bin_day = datetime.strptime(bin_dict['bin_0'][1], "%A %d %B %Y")
+def get_next_bin(bin_dict):    
+    date_format = "%A %d %B %Y"
+    black_date = datetime.strptime(bin_dict['bin_0'][1], date_format)
+    blue_green_date = datetime.strptime(bin_dict['bin_1'][1], date_format)
     
-    # for bin_colour, bin_date in list(bin_dict.values())[1:]:
-    #     day_of_month = datetime.strptime(bin_date, "%A %d %B %Y")
-        
-    #     if day_of_month < next_bin_day:
-    #         next_bin = bin_colour
-    
-    return next_bin[0].upper() + next_bin[1:]
+    bin_options = ["Black", "Blue and green"]
+    bin_dates = [black_date, blue_green_date]
+
+    next_bin_date = min(black_date, blue_green_date)
+    next_bin = bin_options[bin_dates.index(next_bin_date)]
+
+    # now that we have the next bin date, we need to make sure it isn't past that date already
+    today = datetime.now()
+
+    # after 7am on the day of collection
+    if ((today - next_bin_date).seconds) / 3600 > 7:
+        index = bin_options.index(next_bin) - 1
+        next_bin = bin_options[index]
+        next_bin_date = bin_dates[index]
+
+    return f"Next bin = {next_bin}, collection date is {next_bin_date.strftime(date_format)}"
             
 def read_config(string):
-    with open(f'{os.path.dirname(os.path.realpath(__file__))}/config.txt', 'r') as f:
+    with open(f'{os.path.dirname(os.path.realpath(__file__))}/config_priv.txt', 'r') as f:
         params = f.readlines()
     
     return [i.split(" = ")[1] for i in params if string in i][0].strip("\n")
 
 if __name__ == "__main__":
-    # directory of this file
-    global_dir = f"{os.path.dirname(os.path.realpath(__file__))}"
-
-    # website with bin day information
-    bin_day_url = read_config("bin_day_url")
-
-    # user's post code and address
-    post_code = read_config("post_code")
-    address = read_config("address")
-
-    # firefox browser object
-    service = Service(f"{global_dir}/drivers/geckodriver.exe")
-    firefox_options = webdriver.FirefoxOptions()
-    firefox_options.binary_location = read_config("firefox_path")
-    browser = webdriver.Firefox(service=service, options=firefox_options)
-
-    # get bin collection dates
-    bin_dict = get_bin_day_info(browser, post_code, address)
-
-    # email result to given email address
-    # bin_dict = {'bin_0': ('black', 'Tuesday 11 June 2024'), 'bin_1': ('blue', 'Tuesday 18 June 2024'), 'bin_2': ('green', 'Tuesday 18 June 2024')}
-    user_gmail = read_config("user_gmail")
-    user_receive = read_config("user_receive")
-    gmail_app_passwd = read_config("gmail_app_passwd")
-    send_bin_dates_to_email(user_gmail, user_receive, gmail_app_passwd, bin_dict)
-
-
-
+    main()
+    
